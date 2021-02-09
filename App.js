7ad37f019ab328a159c118c4a091af2d87c38948
Minerva Morales <minervamoralesruiz@gmail.com>
2021-02-09 11:56:50 -0400

    creating project

--- App.js
+++ App.js
@@ -1,14 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {HubConnectionBuilder, LogLevel, HttpTransportType} from '@aspnet/signalr';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+class App extends React.Component {
+
+  _hubConnection = new HubConnectionBuilder()
+    .withUrl("https://localhost:44353/chatHub")
+    .configureLogging(LogLevel.Debug)
+    .build();
+
+    componentDidMount(){
+      this._hubConnection.start().then(a => {
+        console.log('Connected rafa');
+      });
+      this._hubConnection.on('ReceiveMessage', message => {
+        console.log("message",message);
+      });
+
+      this._hubConnection.on('ReceiveStores', stores => {
+        console.log(stores);
+      });
+    }
+
+    render(){
+      return (
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
+      );
+    }
+ 
 }
 
 const styles = StyleSheet.create({
@@ -19,3 +43,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+export default App;
